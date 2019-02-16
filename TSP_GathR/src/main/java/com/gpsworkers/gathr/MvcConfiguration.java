532fae0ef686303cfc23abd2967553b338e4e256
aplarkin1 <46717895+aplarkin1@users.noreply.github.com>
2019-02-16 12:37:48 -0500

    Testing from Alex's Laptop

---
+++ TSP_GathR/src/main/java/com/gpsworkers/gathr/MvcConfiguration.java
@@ -0,0 +1,22 @@
+package com.gpsworkers.gathr;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer{
+	
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/info").setViewName("info");
+		registry.addViewController("/secured").setViewName("secured");
+	}
+	
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/"); 
+	}
+	
+}
