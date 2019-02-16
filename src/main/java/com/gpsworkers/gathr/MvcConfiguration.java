329d1ad27e6352682c8e539b3e872d7e544ec134
Alexander Larkin <s4galexlarkin@hotmail.com>
2019-02-16 11:09:49 -0500

    Spring Security Basics have been setup...only authenticated users can access info and secured.  Any user, authenticated or not can access login and home...just as planned....Also added a few default html pages. Added the MVC configuration and Security Configuration files.

---
+++ src/main/java/com/gpsworkers/gathr/MvcConfiguration.java
@@ -0,0 +1,17 @@
+package com.gpsworkers.gathr;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer{
+	
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/info").setViewName("info");
+		registry.addViewController("/secured").setViewName("secured");
+	}
+	
+}
---
+++ src/main/java/com/gpsworkers/gathr/WebSecurityConfiguration.java
@@ -0,0 +1,45 @@
+package com.gpsworkers.gathr;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/", "/home").permitAll().anyRequest().anonymous()
+                .antMatchers("/secured", "/info").permitAll().anyRequest().authenticated()                
+                .and().formLogin();
+
+        // Commented out because we don't have a non-default login page as of yet
+
+                /*.loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+                */
+    }
+    
+    
+    //TYhis is commented out because we haven't created our Mongo Connection yet/User Details Service
+    /*
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+             User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+    */
+}
