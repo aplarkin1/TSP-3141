3fcda9bddbde27c661ebbb7e983654635b9f224f
Alexander Larkin <s4galexlarkin@hotmail.com>
2019-02-27 10:27:25 -0500

    Test Cases

--- src/test/java/com/gpsworkers/gathr/LocationUpdatingTests.java
+++
@@ -1,5 +0,0 @@
-package com.gpsworkers.gathr;
-
-public class LocationUpdatingTests {
-
-}
---
+++ src/test/java/com/gpsworkers/gathr/WebTests.java
@@ -0,0 +1,37 @@
+package com.gpsworkers.gathr;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.gpsworkers.gathr.mongo.users.UserRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class WebTests {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	@Autowired
+	UserRepository userRepo;
+	
+	@Test
+	public void testInfoRedirectToLoginPageWithout() throws Exception {
+		String url = "/info";
+		String loginContents = this.restTemplate.getForObject("http://localhost:" + port + "/login", String.class);
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + url, String.class)).contains("<title>Login</title>");
+		//System.out.println();
+	}
+	
+}
