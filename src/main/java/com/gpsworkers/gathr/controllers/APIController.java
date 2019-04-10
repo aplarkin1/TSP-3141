0594e8b78c19d3fb0a48937e8a764e20861e1b22
aplarkin1 <46717895+aplarkin1@users.noreply.github.com>
2019-04-10 11:25:11 -0400

    Here

--- src/main/java/com/gpsworkers/gathr/controllers/APIController.java
+++ src/main/java/com/gpsworkers/gathr/controllers/APIController.java
@@ -360,7 +360,7 @@ public class APIController {
 			return new ResponseEntity<>("-2", HttpStatus.NOT_FOUND);
 		}
 	}
-
+//
 	@GetMapping("/api/getLocationsOfGroupMembers")
 	@ResponseBody
 	public ResponseEntity<String> handleGetLocationsOfGroupMembers(String groupId) throws JsonProcessingException {
