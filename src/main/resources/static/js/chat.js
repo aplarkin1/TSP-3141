4273e45cd0adca1621ad3b292a38048e5782b00b
aplarkin1 <46717895+aplarkin1@users.noreply.github.com>
2019-04-10 11:35:18 -0400

    Chat.js made

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -6,6 +6,20 @@ function getMessagesOfGroup(){
 	
 }
 
-function getAllGroupsAndLocationsOfEachGroupMember(){
-	
+function getAllGroupNamesOfUser(){
+	$.get("/api/getAllGroupNames").done(function(data) {
+		alert( "Got the group names" );
+		console.log(data)
+	}).fail(function() {
+		alert( "error" );
+	})
+}
+
+function getAllGroupMemberLocations(){
+	$.get("/api/getLocationsOfGroupMembers").done(function(data) {
+		alert( "Got the locations of group members" );
+		console.log(data)
+	}).fail(function() {
+		alert( "error" );
+	})
 }
